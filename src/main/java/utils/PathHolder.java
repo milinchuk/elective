c1db6f6c121ecb1ce2fa5e0dca73372357e316ab
milinchuk <anastasia.milinchuk@gmail.com>
2016-11-21 16:22:43 +0200

    Added attributes text constants

--- src/main/java/utils/PathHolder.java
+++ src/main/java/utils/PathHolder.java
@@ -7,4 +7,7 @@ public interface PathHolder {
     String PREFIX = "/WEB-INF";
     String COURSES = "/WEB-INF/courses.jsp";
     String PAGE_NOT_FOUND_JSP = "/WEB-INF/pageNotFound.jsp";
+    String MY_COURSES = "/WEB-INF/myCourses.jsp";
+    String STUDENTS = "/WEB-INF/students.jsp";
+    String PROFILE = "/WEB-INF/profile.jsp";
 }
--- src/main/java/utils/UrlHolder.java
+++ src/main/java/utils/UrlHolder.java
@@ -4,14 +4,15 @@ package utils;
  * Created by click on 11/21/2016.
  */
 public interface UrlHolder {
-    public static final String SUFFIX = "/";
-    public static final String PREFIX = "/user";
-    public static final String COURSES = PREFIX + "/courses";
-    public static final String COURSE = PREFIX + "/course/";
-    public static final String STUDENTS = PREFIX + "/students/";
-    public static final String STUDENT = PREFIX + "/student/";
-    public static final String LOGIN = "/login";
-    public static final String SIGNUP = "/signup";
-    public static final String PROFILE = PREFIX + "/profile";
-    public static final String MY_COURSES = PREFIX + "/myCourses";
+    String SUFFIX = "/";
+    String PREFIX = "/user";
+    String COURSES = PREFIX + "/courses";
+    String COURSE = PREFIX + "/course";
+    String STUDENTS = PREFIX + "/students";
+    String STUDENT = PREFIX + "/student";
+    String LOGIN = "/login";
+    String SIGNUP = "/signup";
+    String PROFILE = PREFIX + "/profile";
+    String MY_COURSES = PREFIX + "/myCourses";
+    String LOGOUT = PREFIX + "/logout";
 }
