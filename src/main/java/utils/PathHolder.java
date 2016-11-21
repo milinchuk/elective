8fb305edd2fcd14aaeac4719b30e7e3a69d76cce
milinchuk <anastasia.milinchuk@gmail.com>
2016-11-21 15:34:44 +0200

    Added text constants for paths and urls

---
+++ src/main/java/utils/PathHolder.java
@@ -0,0 +1,10 @@
+package utils;
+
+/**
+ * Created by click on 11/21/2016.
+ */
+public interface PathHolder {
+    String PREFIX = "/WEB-INF";
+    String COURSES = "/WEB-INF/courses.jsp";
+    String PAGE_NOT_FOUND_JSP = "/WEB-INF/pageNotFound.jsp";
+}
---
+++ src/main/java/utils/UrlHolder.java
@@ -0,0 +1,17 @@
+package utils;
+
+/**
+ * Created by click on 11/21/2016.
+ */
+public interface UrlHolder {
+    public static final String SUFFIX = "/";
+    public static final String PREFIX = "/user";
+    public static final String COURSES = PREFIX + "/courses";
+    public static final String COURSE = PREFIX + "/course/";
+    public static final String STUDENTS = PREFIX + "/students/";
+    public static final String STUDENT = PREFIX + "/student/";
+    public static final String LOGIN = "/login";
+    public static final String SIGNUP = "/signup";
+    public static final String PROFILE = PREFIX + "/profile";
+    public static final String MY_COURSES = PREFIX + "/myCourses";
+}
