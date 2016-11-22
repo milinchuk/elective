f1b00fd690cb1797b76fac9c26ccb24e10d15db1
milinchuk <anastasia.milinchuk@gmail.com>
2016-11-22 19:06:58 +0200

    some changes for logout

--- src/main/java/controller/commands/authorization/LogoutCommand.java
+++ src/main/java/controller/commands/authorization/LogoutCommand.java
@@ -1,9 +1,11 @@
 package controller.commands.authorization;
 
 import controller.commands.Command;
+import utils.PagesHolder;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by click on 11/18/2016.
@@ -11,6 +13,8 @@ import javax.servlet.http.HttpServletResponse;
 public class LogoutCommand implements Command {
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) {
-        return null;
+        // clear session
+        //request.getSession().invalidate();
+        return PagesHolder.LOGIN;
     }
 }
