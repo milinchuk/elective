e14a127dcc501111f76a46149076c37654291264
milinchuk <anastasia.milinchuk@gmail.com>
2016-11-22 19:09:59 +0200

    command for my_courses with dividing on student and tutor

---
+++ src/main/java/controller/commands/course/UserCoursesCommand.java
@@ -0,0 +1,47 @@
+package controller.commands.course;
+
+import controller.commands.Command;
+import model.entity.Course;
+import model.entity.Progress;
+import model.entity.User;
+import model.service.CourseServiceImpl;
+import model.service.ProgressServiceImpl;
+import model.service.interfaces.CourseService;
+import model.service.interfaces.ProgressService;
+import utils.AttributesHolder;
+import utils.PagesHolder;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+/**
+ * Created by click on 11/22/2016.
+ */
+public class UserCoursesCommand implements Command {
+    CourseService courseService = CourseServiceImpl.getInstance();
+    ProgressService progressService = ProgressServiceImpl.getInstance();
+
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) {
+        // check role
+        Integer id = Integer.valueOf(String.valueOf(request.getSession().getAttribute(AttributesHolder.ID)));
+        Integer role = Integer.valueOf(String.valueOf(request.getSession().getAttribute(AttributesHolder.ROLE)));
+
+        // if tutor
+        if (role == User.TUTOR) {
+            List<Course> courses = courseService.findByTutor(id);
+            request.setAttribute(AttributesHolder.COURSES, courses);
+            return PagesHolder.TUTOR_COURSES;
+        }
+
+        // if student
+        if (role == User.STUDENT) {
+            List<Progress> progresses = progressService.findByUser(id);
+            request.setAttribute(AttributesHolder.PROGRESSES, progresses);
+            return PagesHolder.STUDENT_COURSES;
+        }
+
+        return PagesHolder.LOGIN;
+    }
+}
