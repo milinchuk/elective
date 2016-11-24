c8fcacefbf930063f7e13fffb21cb4beac270eed
milinchuk <anastasia.milinchuk@gmail.com>
2016-11-24 16:56:57 +0200

    add CommandInit

--- src/main/java/controller/DispatcherServlet.java
+++ src/main/java/controller/DispatcherServlet.java
@@ -3,17 +3,20 @@ package controller;
  * Created by click on 11/5/2016.
  */
 import controller.commands.Command;
-import controller.commands.CommandHolder;
+import controller.commands.holder.CommandHolder;
 import controller.commands.authorization.LoginCommand;
 import controller.commands.authorization.LogoutCommand;
 import controller.commands.authorization.SignUpCommand;
 import controller.commands.course.*;
+import controller.commands.holder.CommandsInit;
+import controller.commands.progress.CourseUnfollowCommand;
 import controller.commands.progress.ProgressStudentsCommand;
 import controller.commands.progress.ProgressUpdateCommand;
 import controller.commands.user.ProfileCommand;
 import controller.commands.user.UpdateProfileCommand;
 import controller.security.Permission;
-import utils.UrlHolder;
+import utils.constants.AttributesHolder;
+import utils.constants.UrlHolder;
 import validators.*;
 import java.io.IOException;
 import java.util.HashMap;
@@ -34,37 +37,9 @@ public class DispatcherServlet extends HttpServlet {
     @Override
     public void init() throws ServletException {
         super.init();
-        Map<String, Command> getCommand = new HashMap<>();
-        Map<String, Command> postCommand = new HashMap<>();
-        // ---------- get commands
-        // for unregister
-        getCommand.put(UrlHolder.LOGIN, new LoginCommand());
-        getCommand.put(UrlHolder.SIGNUP, new SignUpCommand());
-
-        // for register
-        getCommand.put(UrlHolder.PROFILE, new ProfileCommand());
-        getCommand.put(UrlHolder.MY_COURSES, new UserCoursesCommand());
-        getCommand.put(UrlHolder.FIND, new FindCoursesCommand());
-        getCommand.put(UrlHolder.STUDENTS + UrlHolder.SUFFIX, new ProgressStudentsCommand());
-        getCommand.put(UrlHolder.LOGOUT, new LogoutCommand());
-        getCommand.put(UrlHolder.COURSE, new OpenCourseCommand());
-        getCommand.put(UrlHolder.COURSE_ADD, new CourseCommand());
-
-        // ----------- post commands
-        // create
-        postCommand.put(UrlHolder.COURSE_ADD, new CourseCreateCommand());
-        postCommand.put(UrlHolder.FOLLOW, new CourseFollowCommand());
-        // update
-        postCommand.put(UrlHolder.PROFILE, new UpdateProfileCommand());
-        postCommand.put(UrlHolder.COURSE_EDIT, new CourseUpdateCommand());
-        postCommand.put(UrlHolder.STUDENT_EDIT, new ProgressUpdateCommand());
-        // delete
-        postCommand.put(UrlHolder.UNFOLLOW, new CourseFollowCommand());
-        postCommand.put(UrlHolder.COURSE_DELETE, new CourseDeleteCommand());
-
         commandHolder = new CommandHolder();
-        commandHolder.setGetCommands(getCommand);
-        commandHolder.setPostCommands(postCommand);
+        CommandsInit commandsInit = new CommandsInit();
+        commandsInit.initCommandHolder(commandHolder);
     }
 
     @Override
@@ -74,29 +49,28 @@ public class DispatcherServlet extends HttpServlet {
         Permission permission = new Permission();
         permission.hasPermission(request);
 
-        // check on FILE NOT FOUND
-        Command command = commandHolder.getGetCommand(request.getRequestURI());
-        Validator commandValidator = new CommandValidator();
-        if(!commandValidator.validate(command)){
-            command = commandHolder.getPageNotFoundCommand();
-        }
-
-        RequestDispatcher dispatcher = request.getRequestDispatcher(command.execute(request, response));
+        Command command = commandHolder.getGetCommand(
+                String.valueOf(request.getSession().getAttribute(AttributesHolder.ROLE))
+                        + request.getRequestURI());
+        RequestDispatcher dispatcher = doRequest(request, response, command);
         dispatcher.forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        RequestDispatcher dispatcher = doRequest(request, response);
-        dispatcher.forward(request, response);
-    }
-
-    protected RequestDispatcher doRequest(HttpServletRequest request, HttpServletResponse response){
+        // imitate authorization
         Permission permission = new Permission();
         permission.hasPermission(request);
 
-        Command command = commandHolder.getPostCommand(request.getRequestURI());
+        Command command = commandHolder.getPostCommand(
+                String.valueOf(request.getSession().getAttribute(AttributesHolder.ROLE))
+                + request.getRequestURI());
+        RequestDispatcher dispatcher = doRequest(request, response, command);
+        dispatcher.forward(request, response);
+    }
+
+    protected RequestDispatcher doRequest(HttpServletRequest request, HttpServletResponse response, Command command){
         Validator commandValidator = new CommandValidator();
         if(!commandValidator.validate(command)){
             command = commandHolder.getPageNotFoundCommand();
