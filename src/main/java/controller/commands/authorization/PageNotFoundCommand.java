85f2699a344e3d2011e708fe320dcbf6d8b6408e
milinchuk <anastasia.milinchuk@gmail.com>
2016-11-25 01:54:29 +0200

    Page not found update

--- src/main/java/controller/commands/authorization/PageNotFoundCommand.java
+++ src/main/java/controller/commands/authorization/PageNotFoundCommand.java
@@ -1,18 +1,20 @@
 package controller.commands.authorization;
 
 import controller.commands.Command;
+import utils.constants.PagesHolder;
+import utils.constants.UrlHolder;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by click on 11/20/2016.
  */
 public class PageNotFoundCommand implements Command {
-    public static final String PAGE_NOT_FOUND = "/WEB-INF/pageNotFound.jsp";
-
     @Override
-    public String execute(HttpServletRequest request, HttpServletResponse response) {
-        return PAGE_NOT_FOUND;
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        return PagesHolder.PAGE_NOT_FOUND_JSP;
     }
 }
