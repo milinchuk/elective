f5b741595b311e2cea05273064d88fcc728af248
milinchuk <anastasia.milinchuk@gmail.com>
2016-12-16 13:22:12 +0200

    Added comments and bug fixed

--- src/test/java/controller/DispatcherServletTest.java
+++ src/test/java/controller/DispatcherServletTest.java
@@ -6,11 +6,4 @@ import org.junit.Before;
  * Created by click on 12/7/2016.
  */
 public class DispatcherServletTest {
-    @Before
-    public void c(){
-
-    }
-
-    @Before
-    public void d(){}
 }
