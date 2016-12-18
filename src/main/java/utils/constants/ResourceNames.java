1d50b4afaffa7004a062784afe02091c8d8fa3f5
milinchuk <anastasia.milinchuk@gmail.com>
2016-12-19 00:01:47 +0200

    refactoring

--- src/main/java/utils/constants/ResourceNames.java
+++ src/main/java/utils/constants/ResourceNames.java
@@ -4,9 +4,9 @@ package utils.constants;
  * Created by click on 12/2/2016.
  */
 public interface ResourceNames {
-    String COURSE_QUERIES_URL = "course_query.properties";
-    String USER_QUERIES_URL = "user_query.properties";
-    String PROGRESS_QUERIES_URL = "progress_query.properties";
-    String REGEX = "regex.properties";
+    String COURSE_QUERIES_URL = "db_queries/course_query.properties";
+    String USER_QUERIES_URL = "db_queries/user_query.properties";
+    String PROGRESS_QUERIES_URL = "db_queries/progress_query.properties";
+    String REGEX = "regex/regex.properties";
     String DATA_SOURCE = "java:comp/env/jdbc/elective";
 }
